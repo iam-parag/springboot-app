ce2fc79c98338653d78d3e33f7d5484a7100c180
iam-parag <94896763+iam-parag@users.noreply.github.com>
2025-01-07 15:08:49 +0530

    Update sql_backup.sql

--- sql_backup.sql
+++ sql_backup.sql
@@ -18,7 +18,7 @@
 --
 -- Table structure for table `phonebook`
 --
-
+USE phonebook;
 DROP TABLE IF EXISTS `phonebook`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
